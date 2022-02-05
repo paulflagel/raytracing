280a41505d50cd5b8f52a0afe605b10f99acc0ef
paulflagel <paulflagel@gmail.com>
2022-02-05 14:48:58 +0100

    Added naive soft shadows

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@
 std::default_random_engine rng[NB_THREADS]; // On en crée 8 pour le calcul parallèle sur 8 threads
 std::uniform_real_distribution<double> uniform(0.0, 1.0);
 
-double epsilon = 0.01;
+const float epsilon = 0.01;
 
 // & : au lieu de passer en paramètre le vecteur soit 3 fois 64 bits, on passe une adresse qui est 1 fois 64 bits
 
@@ -100,13 +100,14 @@ public:
 class Sphere
 {
 public:
-    explicit Sphere(const Vector &origin, double radius, const Vector &albedo, bool mirror = false, bool transparent = false, double refraction_index = 1.4)
+    explicit Sphere(const Vector &origin, double radius, const Vector &albedo, bool isMirror = false, bool isTransp = false, bool isLight = false, double refraction_index = 1.4)
     {
         this->O = origin;
         this->R = radius;
         this->albedo = albedo;
-        this->mirror = mirror;
-        this->transp = transparent;
+        this->isMirror = isMirror;
+        this->isTransp = isTransp;
+        this->isLight = isLight;
         this->refraction_index = refraction_index;
     }
 
@@ -147,7 +148,7 @@ public:
     Vector O;
     double R;
     Vector albedo;
-    bool mirror, transp;
+    bool isMirror, isTransp, isLight;
     double refraction_index;
 };
 
@@ -237,8 +238,14 @@ public:
 
         if (this->intersect(r, P, n, id_sphere, t))
         {
+            // Sphère de lumière
+            if (this->liste_spheres[id_sphere].isLight)
+            {
+                return this->liste_spheres[id_sphere].albedo * this->I / (4 * M_PI * this->liste_spheres[id_sphere].R * this->liste_spheres[id_sphere].R);
+            }
+
             //  SURFACE MIROIR :
-            if (this->liste_spheres[id_sphere].mirror)
+            if (this->liste_spheres[id_sphere].isMirror)
             {
                 Vector uReflected = r.u - 2 * dot(r.u, n) * n;
                 Ray rReflected(P + (epsilon * n), uReflected);
@@ -246,7 +253,7 @@ public:
             }
 
             // SURFACE TRANSPARENTE :
-            else if (this->liste_spheres[id_sphere].transp)
+            else if (this->liste_spheres[id_sphere].isTransp)
             {
                 double dot_incident_normal = dot(r.u, n);
                 double n1, n2;
@@ -293,20 +300,26 @@ public:
                 Vector Plum, nlum;
                 double tlum;
                 int idlum;
-                int visibility;
-
+                int visibility = 1; // Pas d'ombre portée
+                                    /*
                 // ECLAIRAGE DIRECT
-                if (this->intersect(Ray(P + (epsilon * n), l), Plum, nlum, idlum, tlum) && tlum < distlum) // Calcul d'ombre portée
+
+                // Calcul d'ombre portée
+                if (this->intersect(Ray(P + (epsilon * n), l), Plum, nlum, idlum, tlum) && tlum < distlum)
                 {
                     visibility = 0;
                 }
-                else // Pas d'ombre portée, on fait le calcul normal de couleur
-                {
-                    visibility = 1;
-                }
 
-                color = visibility * this->liste_spheres[id_sphere].albedo * this->I * std::max(0., dot(l, n)) / (lnorm2 * 4 * M_PI * M_PI); // clamp à 0 pour pas avoir une intensité négative
+                // // Soft shadows
+                // Vector V = -1 * l;
+                // Vector omega = random_cos(V);
+                // Vector xprime = omega * this->liste_spheres[0].R + L;
+                // Vector nprime = (xprime - L) / (xprime - L).norm();
+                // double pdf = dot(l, omega) / (M_PI * this->liste_spheres[0].R * this->liste_spheres[0].R);
+                // double jacobien = M_PI * std::max(0., dot(omega, n)) * std::max(0., dot(-1 * omega, nprime)) / ((xprime - P).norm2() * std::max(0., dot(V, nprime)));
 
+                color = 1 * visibility * this->liste_spheres[id_sphere].albedo * this->I * std::max(0., dot(l, n)) / (lnorm2 * 4 * M_PI * M_PI); // clamp à 0 pour pas avoir une intensité négative
+*/
                 // ECLAIRAGE INDIRECT
                 Vector omega_i = random_cos(n);
                 Ray rRandom(P + (epsilon * n), omega_i);
@@ -321,6 +334,7 @@ public:
 };
 
 // Génère deux nombres aléatoires suivant une loi normale centrée réduite à partir de deux nombres suivant une loi uniforme
+const float sigma_bm = 0.3; // Std deviation of the normal distribution (Box Muller)
 void box_muller(double &dx, double &dy)
 {
     int thread_id = omp_get_thread_num();
@@ -328,8 +342,8 @@ void box_muller(double &dx, double &dy)
     double r2 = uniform(rng[thread_id]);
     double r = sqrt(-2 * log(r2));
     double t = 2 * M_PI * r1;
-    dx = cos(t) * r;
-    dy = sin(t) * r;
+    dx = cos(t) * r * sigma_bm;
+    dy = sin(t) * r * sigma_bm;
 };
 
 int main()
@@ -341,44 +355,41 @@ int main()
     Vector L(-10, 20, 40);        // Source de lumière
     double fov = 60 * M_PI / 180; // Field of view 60°
     double tanfov2 = tan(fov / 2);
-    double I(10000000000);         // Intensité de la lumière
-    int nb_rays_monte_carlo = 512; // Nombre de rayons envoyés pour Monte Carlo
+    double I(10000000000);        // Intensité de la lumière
+    int nb_rays_monte_carlo = 64; // Nombre de rayons envoyés pour Monte Carlo
     int rebonds = 5;
-    // int rayons_indirects = 5;
 
     Scene scene(I, L);
 
     // Sphere 1 à l'origine, de rayon 10
     Sphere s1(Vector(0, 0, 0), 10, Vector(0.4, 0.1, 0.), false, false);
     // Sphere 2
-    Sphere s2(Vector(-20, 15, -20), 5, Vector(0.3, 0., 0.3), false, false);
-    // Sphere 3
-    Sphere s3(Vector(20, -2, 5), 3, Vector(0.3, 0.3, 0.1), false, false);
+    Sphere s2(Vector(-25, 20, -35), 10, Vector(0.4, 0.4, 0.4), false, false);
     // Sphère derrière la boule
-    Sphere sBack(Vector(0, 0, -1000), 940, Vector(0., 1., 0.));
+    Sphere sBack(Vector(0, 0, -1000), 940, Vector(0., 0.5, 0.));
     // Sphère derrière la caméra
-    Sphere sFront(Vector(0, 0, 1000), 940, Vector(1., 0., 1.));
+    Sphere sFront(Vector(0, 0, 1000), 940, Vector(0.5, 0., 0.5));
     // Plafond
-    Sphere sUp(Vector(0, 1000, 0), 940, Vector(1., 0., 0.));
+    Sphere sUp(Vector(0, 1000, 0), 940, Vector(0.5, 0., 0.));
     // Sol
-    Sphere sDown(Vector(0, -1000, 0), 990, Vector(0., 0., 1.));
+    Sphere sDown(Vector(0, -1000, 0), 990, Vector(0., 0., 0.5));
     // Mur droit
-    Sphere sRight(Vector(1000, 0, 0), 940, Vector(1., 1., 1.));
+    Sphere sRight(Vector(1000, 0, 0), 940, Vector(0.5, 0.5, 0.5));
     // Mur gauche
-    Sphere sLeft(Vector(-1000, 0, 0), 940, Vector(1., 1., 1.));
+    Sphere sLeft(Vector(-1000, 0, 0), 940, Vector(0.5, 0.5, 0.5));
+
     // Sphere de lumiere
-    Sphere sLum(L, 10, Vector(0., 0., 0.));
+    Sphere sLum(L, 10, Vector(1., 1., 1.), false, false, true);
+    scene.add(sLum);
 
     scene.add(s1);
     scene.add(s2);
-    scene.add(s3);
     scene.add(sFront);
     scene.add(sBack);
     scene.add(sUp);
     scene.add(sDown);
     scene.add(sRight);
     scene.add(sLeft);
-    //scene.add(sLum);
 
     auto start = std::chrono::high_resolution_clock::now();
     std::vector<unsigned char> image(W * H * 3, 0); // Crée un tableau 1D de W*H*3 éléments initialisés à 0 (l'image)
@@ -391,11 +402,11 @@ int main()
             for (int k = 0; k < nb_rays_monte_carlo; k++)
             {
                 //Antialiasing : Plutôt que d'envoyer le rayon pile au centre des pixels, on l'envoie aléatoirement dans le pixel
-                double x = 0.0;
-                double y = 0.0;
-                box_muller(x, y);
+                double dx = 0.0;
+                double dy = 0.0;
+                box_muller(dx, dy);
 
-                Vector u(j - W / 2 + 0.5 + x, (H - i) - (H / 2) + 0.5 + y, -W / (2 * tanfov2)); // On ajoute 0.5 pour être au centre des pixels plutôt que dans les coins
+                Vector u(j - W / 2 + 0.5 + dx, (H - i) - (H / 2) + 0.5 + dy, -W / (2 * tanfov2)); // On ajoute 0.5 pour être au centre des pixels plutôt que dans les coins
                 u.normalize();
                 Ray r(C, u); // Rayon issu de C dans la direction u
                 intensity = intensity + scene.getColor(r, rebonds);
